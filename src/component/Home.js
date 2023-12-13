1f8917915bcc0daf6c2da865f4e7b7a308054eeb
ashish kumar <akumar.mt05@gmail.com>
2023-12-13 17:59:20 +0530

    ui using bootstrap and css

---
+++ src/component/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import Content from './Content'
+
+export default function Home() {
+    return (
+        <div>
+            <Navbar />
+            <Content />
+            <Footer />
+        </div>
+    )
+}
